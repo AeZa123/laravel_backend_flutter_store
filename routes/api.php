34805e52175ef6751a03929449027b184f9d3ec7
Suriya Rabalert <aymexe1212@gmail.com>
2024-04-02 10:42:10 +0700

    update controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,26 @@ use App\Http\Controllers\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::post('logout', [AuthController::class, 'logout']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::Group(['middleware' => 'auth:sanctum'], function(){
+
+    // get all users
+    Route::get('users', [UserController::class, 'index']);
+
+
+    
+
+
+
+
+
+    // Route::resource('products', ProductController::class);
+    // Route::get('products/search/{keyword}', [ProductController::class, 'search']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    // Route::post('logout', [AuthController::class, 'logout']);
+
 });
